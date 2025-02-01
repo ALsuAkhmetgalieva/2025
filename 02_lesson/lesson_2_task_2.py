3c3a0bfc887426bb211ee682dff10fb83044bb77
ALsuAkhmetgalieva <Akhmetgalieva_alsu@mail.ru>
2025-02-01 15:26:47 +0300

    git commit --

--- 02_lesson/lesson_2_task_2.py
+++ 02_lesson/lesson_2_task_2.py
@@ -1,5 +1,6 @@
-def is_year_leap(number):
-    return "True" if number % 4 == 0 else "False"
+def is_year_leap(num):
+    return "True" if num % 4 == 0 else "False"
+
 
 num = int(input("Введите год: "))
 result = is_year_leap(num)
