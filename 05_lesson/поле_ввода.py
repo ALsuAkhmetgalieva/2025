9bad1024d0b9cab754dd400695a42b20703ed711
ALsuAkhmetgalieva <Akhmetgalieva_alsu@mail.ru>
2025-02-16 01:30:20 +0300

    git --commit

---
+++ 05_lesson/поле_ввода.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+sleep(2)
+
+input_field = driver.find_element(By.CSS_SELECTOR, "input[type='number']")
+
+input_field.send_keys("1000")
+
+sleep(2)
+
+input_field.clear()
+
+input_field.send_keys("999")
+
+sleep(2)
+
+driver.quit()
