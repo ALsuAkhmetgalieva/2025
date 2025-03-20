56d954e09eaf66f555b518f30a93a754b4908d12
ALsuAkhmetgalieva <Akhmetgalieva_alsu@mail.ru>
2025-03-20 22:18:13 +0300

    git --commit

---
+++ 07_lesson/pages_calc/test_calculator.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class CalculatorPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def set_delay(self, delay):
+        input_field = self.driver.find_element(By.CSS_SELECTOR, '#delay')
+        input_field.clear()
+        input_field.send_keys(delay)
+
+    def click_button(self, button_text):
+        button = self.driver.find_element(By.XPATH, f"//div[@class='keys']//span[text()='{button_text}']")
+        button.click()
+
+    def wait_for_spinner_to_disappear(self, timeout=45):
+        WebDriverWait(self.driver, timeout).until_not(
+            EC.visibility_of(self.driver.find_element(By.CSS_SELECTOR, '#spinner'))
+        )
+
+    def get_result(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '.screen').text
---
+++ 07_lesson/pages_shop/shop_page.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class ShopPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def login(self, username, password):
+        self.driver.find_element(By.CSS_SELECTOR, '#user-name').send_keys(username)
+        self.driver.find_element(By.CSS_SELECTOR, '#password').send_keys(password)
+        self.driver.find_element(By.CSS_SELECTOR, '#login-button').click()
+
+    def add_to_cart(self, item_id):
+        self.driver.find_element(By.CSS_SELECTOR, f'#add-to-cart-{item_id}').click()
+
+    def go_to_cart(self):
+        self.driver.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
+
+    def proceed_to_checkout(self):
+        self.driver.find_element(By.CSS_SELECTOR, '#checkout').click()
+
+    def enter_checkout_info(self, first_name, last_name, postal_code):
+        self.driver.find_element(By.CSS_SELECTOR, '#first-name').send_keys(first_name)
+        self.driver.find_element(By.CSS_SELECTOR, '#last-name').send_keys(last_name)
+        self.driver.find_element(By.CSS_SELECTOR, '#postal-code').send_keys(postal_code)
+        self.driver.find_element(By.CSS_SELECTOR, '#continue').click()
+
+    def get_total(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '[data-test="total-label"]').text
